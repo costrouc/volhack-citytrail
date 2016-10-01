7bedb414b9e966821a581e1dcfada76729c2bcc2
Tyler <relyt462@gmail.com>
2016-10-01 07:13:15 -0400

    Added basic icon logic and set up basic styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -23,10 +23,29 @@ var newUserModal = document.getElementById('newUserModal');
 document.getElementById("newUserForm").onsubmit = function(){
     newUserModal.style.display = "none";
     alert('userName = ' + document.getElementById("newUserForm").elements["Username"].value); 
-    return false;
-};
-document.getElementById("newUserForm").onload = function(){
-
+    return false; //Prevent page reload
 };
 
+//Handle getting player icons
+document.getElementsByTagName("BODY")[0].onload = function(){
+    addIcons(document.getElementById("newUserIcons"));
+};
+
+function addIcons(parentDiv){
+    //Replace this with actual icon loading
+    var icons = ["http://www.freeiconspng.com/uploads/person-icon-person-icon-clipart-image-from-our-icon-clipart-category--9.png"];
+    for(var i = 0; i < icons.length; i++)
+    {
+        var curNode = document.createElement("label");
+        var curRadio = document.createElement("input");
+        curRadio.setAttribute("type", "radio");
+        curRadio.setAttribute("name", icons[i].substring(0, icons[i].length - 4));
+        curRadio.setAttribute("value", "small");
+        var curImage = document.createElement("img");
+        curImage.setAttribute("src", icons[i]);
+        curNode.appendChild(curRadio);
+        curNode.appendChild(curImage);
+        parentDiv.appendChild(curNode);
+    }
+};
 
