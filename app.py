b4a2ec7edc9846fec2bf72aafa4026149123ea40
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2016-10-01 15:41:10 -0400

    Adding python flask server

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, send_from_directory, request
+import uuid
+
+app = Flask(__name__)
+
+static_path = 'public'
+
+@app.route('/signup')
+def signup_view():
+    user = request.json()
+    print(user)
+    return "Hello %s , %s" % (user[username], user[icon])
+
+
+
+
+# Hack for static hosting
+@app.route('/')
+def home_view():
+    return send_from_directory(static_path, 'index.html')
+
+@app.route('/scripts/<path:path>')
+@app.route('/stylesheets/<path:path>')
+@app.route('/data/<path:path>')
+def static_assets(path):
+    return send_from_directory(static_path, request.path[1:]) # hack
+
+if __name__ == "__main__":
+    app.run(debug=True)
